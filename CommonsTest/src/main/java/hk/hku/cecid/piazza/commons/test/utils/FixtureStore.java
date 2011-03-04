deb2fb6352ca82f2e5c507d1dd4aaa1454869ba1
Aaron Walker <a.walker@base2services.com>
2011-03-04 16:50:37 +1100

    added Corvus.WSClient and CommonsExt modules and some code behind missing testing code to make a bunch of tests pass

--- CommonsTest/src/main/java/hk/hku/cecid/piazza/commons/test/utils/FixtureStore.java
+++ CommonsTest/src/main/java/hk/hku/cecid/piazza/commons/test/utils/FixtureStore.java
@@ -3,6 +3,11 @@
  */
 package hk.hku.cecid.piazza.commons.test.utils;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Enumeration;
+
 
 /**
  * @author aaronwalker
@@ -10,8 +15,51 @@ package hk.hku.cecid.piazza.commons.test.utils;
  */
 public class FixtureStore {
 
-    public static ClassLoader createFixtureLoader(boolean b, Class<?> class1) {
-        return class1.getClassLoader();
+    public static ClassLoader createFixtureLoader(boolean delegate, Class<?> class1) {
+        return new FixtureClassLoader(delegate,class1);
+    }
+    
+    public static class FixtureClassLoader extends ClassLoader {
+
+        private boolean delegate;
+        private Class<?> parentCL;
+        
+        public FixtureClassLoader(boolean delegate, Class<?> class1) {
+            this.delegate = delegate;
+            this.parentCL = class1;
+        }
+
+        @Override
+        public URL getResource(String resource) {
+            
+            URL url = super.getResource(parentCL.getName() + "/" + resource);
+            if(url == null && delegate) {
+                return super.getResource(resource);
+            }
+            return url;
+        }
+
+        @Override
+        public InputStream getResourceAsStream(String resource) {
+            InputStream is =  super.getResourceAsStream(parentCL.getName() + "/" + resource);
+            if(is == null && delegate) {
+                return super.getResourceAsStream(resource);
+            }
+            return is;
+        }
+
+        @Override
+        public Enumeration<URL> getResources(String resource) throws IOException {
+            
+            Enumeration<URL> urls = super.getResources(parentCL.getName() + "/" + resource);
+            if(urls == null && delegate) {
+                return super.getResources(resource);
+            }
+            return urls;
+        }
+
+        
+        
     }
 
 }
