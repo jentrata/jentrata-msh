bc30a914750fd38b319d80638f51dc6bd81b3259
arran ubels <a.ubels@base2services.com>
2016-06-27 11:27:44 +1000

    ActiveMQ appears to be working.

--- ContainerFiles/run.sh
+++ ContainerFiles/run.sh
@@ -24,8 +24,20 @@ export JENTRATA_AS2_DB_URL="jdbc:postgresql://$DB_HOST_NAME:5432/as2"
 export JENTRATA_AS2_DB_USER="$DB_USER_NAME"
 export JENTRATA_AS2_DB_PASS="$DB_USER_PASS"
 
+export JENTRATA_ACTIVEMQ_MODULE="$ACTIVEMQ_MODE"
+export ACTIVEMQ_HOST_NAME="localhost"
+if test "$ACTIVEMQ_MODE" = "external"; then
+    export ACTIVEMQ_HOST_NAME=`echo "$ACTIVEMQ_URL" | sed -n 's:^tcp\://\([^\:]*\)\:61616:\1:p'`
+    export JENTRATA_ACTIVEMQ_USERNAME="$ACTIVEMQ_USER"
+    export JENTRATA_ACTIVEMQ_PASSWORD="$ACTIVEMQ_PASS"
+    export JENTRATA_ACTIVEMQ_CONNECTIONFACTORYURL="$ACTIVEMQ_URL"
+fi
+
 export JAVA_OPTS="$JAVA_OPTS -Dcorvus.home=$JENTRATA_HOME"
 
+echo waiting for postgres
 /opt/wait-for-it.sh "$DB_HOST_NAME:5432"
+echo waiting for activemq
+/opt/wait-for-it.sh "$ACTIVEMQ_HOST_NAME:61616"
 
 exec catalina.sh run
