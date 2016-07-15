52dbaebb5331331c590ff8e24a3424f9d89c19b3
Aaron Walker <a.walker@base2services.com>
2016-07-15 10:51:15 +1000

    updates internal message delivery status for inbound messages (#30)
    * updates internal message delivery status for inbound messages
    
    * updates example to include examples for sent and acked messages

--- Plugins/CorvusEbMS/src/main/java/hk/hku/cecid/ebms/spa/event/EbmsCamelListener.java
+++ Plugins/CorvusEbMS/src/main/java/hk/hku/cecid/ebms/spa/event/EbmsCamelListener.java
@@ -3,9 +3,14 @@ package hk.hku.cecid.ebms.spa.event;
 import hk.hku.cecid.ebms.pkg.EbxmlMessage;
 import hk.hku.cecid.ebms.pkg.MessageHeader;
 import hk.hku.cecid.ebms.spa.EbmsProcessor;
+import hk.hku.cecid.ebms.spa.dao.MessageDAO;
+import hk.hku.cecid.ebms.spa.dao.MessageDVO;
+import hk.hku.cecid.ebms.spa.dao.MessageServerDAO;
+import hk.hku.cecid.ebms.spa.handler.MessageClassifier;
 import hk.hku.cecid.ebms.spa.task.EbmsEventListener;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
+import org.apache.camel.ExchangePattern;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.impl.DefaultExchange;
@@ -13,6 +18,7 @@ import org.apache.camel.spi.Synchronization;
 
 import javax.xml.soap.AttachmentPart;
 import javax.xml.soap.SOAPMessage;
+import java.io.ByteArrayOutputStream;
 import java.io.Closeable;
 import java.io.IOException;
 import java.util.*;
@@ -36,31 +42,34 @@ public class EbmsCamelListener extends EbmsEventListener implements Closeable {
     protected static final String MSG_PROPERTY_TIMETOLIVE = "ebxml_timestolive";
     protected static final String MSG_PROPERTY_PAYLOAD_ID = "ebxml_payload_id";
     protected static final String MSG_PROPERTY_PAYLOAD_TYPE = "ebxml_payload_type";
+    protected static final String MSG_PROPERTY_REF_MSG_ID = "ebxml_ref_message_id";
 
     private CamelContext camelContext;
     private ProducerTemplate eventHandler;
-    private String endpointOptions = null;
 
     @Override
     protected void init() throws Exception {
         super.init();
         camelContext = new DefaultCamelContext();
+        camelContext.start();
         eventHandler = camelContext.createProducerTemplate();
-        endpointOptions = getEndpointOptions();
     }
 
     @Override
     public void messageSent(EbxmlMessage requestMessage) {
+        //publish sent event
         send(requestMessage, "vm:ebmsSent");
     }
 
     @Override
     public void messageReceived(EbxmlMessage requestMessage) {
-        send(requestMessage, "vm:ebmsReceived");
+        //we only want to support a single inbound delivery endpoint
+        send(requestMessage, "direct-vm:ebmsReceived", "?failIfNoConsumers=false");
     }
 
     @Override
     public void responseReceived(EbxmlMessage acknowledgement) {
+        //publish ack received event
         send(acknowledgement, "vm:ebmsAckReceived");
     }
 
@@ -81,24 +90,59 @@ public class EbmsCamelListener extends EbmsEventListener implements Closeable {
     }
 
     private void send(EbxmlMessage ebxml, final String endpoint) {
+        send(ebxml,endpoint,"?discardIfNoConsumers=true&multipleConsumers=true");
+    }
+
+    private void send(EbxmlMessage ebxml, final String endpoint, final  String options) {
         try {
             List<Exchange> exchanges = convertEbxmlMessage(ebxml);
             for(Exchange exchange: exchanges) {
-                eventHandler.asyncCallback(endpoint + endpointOptions, exchange, new Synchronization() {
+                exchange.getIn().setHeader("ebmsEvent", endpoint);
+                eventHandler.asyncCallback(endpoint + options, exchange, new Synchronization() {
                     @Override
                     public void onComplete(Exchange exchange) {
                         EbmsProcessor.core.log.info(endpoint + ":successfully sent:" + exchange.getIn().getHeader(MSG_PROPERTY_MSG_ID));
+                        if("direct-vm:ebmsReceived".equals(exchange.getIn().getHeader("ebmsEvent"))) {
+                            updateDeliveryStatus(exchange.getIn().getHeader(MSG_PROPERTY_MSG_ID,String.class),
+                                    MessageClassifier.INTERNAL_STATUS_DELIVERED,
+                                    "Message is delivered");
+                        }
                     }
 
                     @Override
                     public void onFailure(Exchange exchange) {
                         EbmsProcessor.core.log.warn(endpoint + ":failed to send:" + exchange.getIn().getHeader(MSG_PROPERTY_MSG_ID));
+                        if("direct-vm:ebmsReceived".equals(exchange.getIn().getHeader("ebmsEvent"))) {
+                            updateDeliveryStatus(exchange.getIn().getHeader(MSG_PROPERTY_MSG_ID,String.class),
+                                    MessageClassifier.INTERNAL_STATUS_DELIVERY_FAILURE,
+                                    "Failed Delivery:" + exchange.getException());
+                        }
                     }
                 });
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private void updateDeliveryStatus(String messageId, String status, String description) {
+        try {
+            MessageDAO messageDao = (MessageDAO) EbmsProcessor.core.dao.createDAO(MessageDAO.class);
+            MessageDVO messageDvo = (MessageDVO) messageDao.createDVO();
+            messageDvo.setMessageId(messageId);
+            messageDvo.setMessageBox(MessageClassifier.MESSAGE_BOX_INBOX);
+            if (messageDao.findMessage(messageDvo)) {
+                if (messageDvo.getStatus().equals(MessageClassifier.INTERNAL_STATUS_PROCESSED)) {
+                    MessageServerDAO messageServerDao = (MessageServerDAO) EbmsProcessor.core.dao.createDAO(MessageServerDAO.class);
+                    messageDvo.setStatus(status);
+                    messageDvo.setStatusDescription(description);
+                    messageServerDao.clearMessage(messageDvo);
+                }
+            }
+        } catch (Exception e) {
+            EbmsProcessor.core.log.error("",e);
+        }
+
     }
 
     private List<Exchange> convertEbxmlMessage(EbxmlMessage ebxml) throws Exception {
@@ -111,6 +155,7 @@ public class EbmsCamelListener extends EbmsEventListener implements Closeable {
         header.put(MSG_PROPERTY_SERVICE_TYPE, ebxml.getServiceType());
         header.put(MSG_PROPERTY_ACTION, ebxml.getAction());
         header.put(MSG_PROPERTY_CONV_ID, ebxml.getConversationId());
+        header.put(MSG_PROPERTY_REF_MSG_ID,ebxml.getMessageHeader().getRefToMessageId());
         if (ebxml.getFromPartyIds().hasNext()) {
             MessageHeader.PartyId partyId = (MessageHeader.PartyId) ebxml.getFromPartyIds().next();
             header.put(MSG_PROPERTY_FROM_PARTY_ID,partyId.getId());
@@ -139,17 +184,13 @@ public class EbmsCamelListener extends EbmsEventListener implements Closeable {
             }
         } else {
             DefaultExchange exchange = new DefaultExchange(camelContext);
+            exchange.setPattern(ExchangePattern.InOut);
             exchange.getIn().setHeaders(header);
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            ebxml.writeTo(bos);
+            exchange.getIn().setBody(bos.toByteArray());
             exchanges.add(exchange);
         }
         return exchanges;
-    }
-
-    private String getEndpointOptions() {
-        String options = getParameters().getProperty("endpointOptions","?discardIfNoConsumers=true&multipleConsumers=true");
-        if(options.startsWith("?")) {
-            return options;
-        }
-        return "?" + options;
     }
 }
