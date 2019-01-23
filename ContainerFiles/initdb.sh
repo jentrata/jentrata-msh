3f9ecadaf2ea9ca4764d33cbb4d73572c1b822ef
cavycorp <hans@rinis.nl>
2019-01-23 14:50:50 +0100

    Database user should be jentrata
    As part of the docker-compose.yml file the database owner is set to jentrata i.s.o. the standard postgres user. Which means that this scripts should use jentrata as username.

--- ContainerFiles/initdb.sh
+++ ContainerFiles/initdb.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -ex
 
-psql --username postgres <<-EOSQL
+psql --username jentrata <<-EOSQL
     CREATE USER "$DB_USER_NAME" WITH SUPERUSER PASSWORD '$DB_USER_PASS' ;
 EOSQL
 
-psql --username postgres <<-EOSQL
+psql --username jentrata <<-EOSQL
     CREATE DATABASE ebms ;
     CREATE DATABASE as2 ;
 EOSQL
 
-cat  /work/sql/ebms.sql | psql --username postgres ebms
-cat  /work/sql/as2.sql | psql --username postgres as2
+cat  /work/sql/ebms.sql | psql --username jentrata ebms
+cat  /work/sql/as2.sql | psql --username jentrata as2
 
